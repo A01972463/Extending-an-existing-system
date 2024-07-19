764433322f5d526522e203ba33d1c4a043c8db30
Gustavo <a01972463@usu.edu>
2024-07-19 14:50:37 -0600

    Initializing ChatGPT references

--- Extending-an-existing-system/src/main/kotlin/Main.kt
+++ Extending-an-existing-system/src/main/kotlin/Main.kt
@@ -12,6 +12,7 @@ import java.io.File
 fun main(args: Array<String>) {
     embeddedServer(Netty, 8080) {
         routing {
+//            Actual Homework Stuff
             get("/") {
                 call.respondText(File("index.html").readText(), ContentType.Text.Html)
             }
@@ -21,6 +22,28 @@ fun main(args: Array<String>) {
                 println(data)
                 call.respondText { "Yay" }
             }
+
+//            ChatGPT stuff
+            get("/gpt") {
+                // Get the user input from query parameters
+                val userInput = call.request.queryParameters["userInput"]
+
+                // Manipulate the input (for example, converting to uppercase)
+                val result = userInput?.uppercase() ?: ""
+
+                // Read the index.html file and replace the placeholder with the result
+                val htmlContent = File("ChatGPT.html").readText().replace("\${'$'}result", result)
+
+                // Respond with the modified HTML content
+                call.respondText(htmlContent, ContentType.Text.Html)
+            }
+
+            post("/gptData") {
+                val data = call.receiveText()
+                println(data)
+                call.respondText("Yay")
+            }
+
         }
     }.start(wait = true)
 }
