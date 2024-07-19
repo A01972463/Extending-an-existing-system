c43b0e13698967717d390a93d0f86214b19f47fb
Gustavo <gustavohuerta96@gmail.com>
2024-07-19 11:23:57 -0600

    Web server servicing

--- Extending-an-existing-system/src/main/kotlin/Main.kt
+++ Extending-an-existing-system/src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.io.File
@@ -13,6 +14,12 @@ fun main(args: Array<String>) {
         routing {
             get("/") {
                 call.respondText(File("index.html").readText(), ContentType.Text.Html)
+            }
+
+            post("/data") {
+                val data = call.receiveText()
+                println(data)
+                call.respondText { "Yay" }
             }
         }
     }.start(wait = true)
